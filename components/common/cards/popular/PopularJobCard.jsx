798fb46bc3568b406e8eca481d8b3fe1058e3ed5
pratayyy <2028026@kiit.ac.in>
2023-03-20 14:32:33 +0530

    first commit

---
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -0,0 +1,30 @@
+import { View, Text, TouchableOpacity, Image } from "react-native";
+
+import styles from "./popularjobcard.style";
+
+export default function PopularJobCard({ item, selectedJob, handleCardPress }) {
+  return (
+    <TouchableOpacity
+      style={styles.container(selectedJob, item)}
+      onPress={() => handleCardPress(item)}
+    >
+      <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
+        <Image
+          source={{ url: item.employer_logo }}
+          resizeMode="contain"
+          style={styles.logoImage}
+        />
+      </TouchableOpacity>
+      <Text style={styles.companyName} numberOfLines={1}>
+        {item.employer_name}
+      </Text>
+
+      <View style={styles.infoContainer}>
+        <Text style={styles.jobName(selectedJob, item)} numberOfLines={1}>
+          {item.job_title}
+        </Text>
+        <Text styles={styles.location}>{item.job_country}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+}
