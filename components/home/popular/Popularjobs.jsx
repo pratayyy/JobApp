ccf62c63753ac746ac760451fd714e2f69cb381f
pratayyy <2028026@kiit.ac.in>
2023-03-21 09:26:56 +0530

    nearby jobs added

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useRouter } from "expo-router";
 import {
   View,
   Text,
@@ -6,7 +7,6 @@ import {
   FlatList,
   ActivityIndicator,
 } from "react-native";
-import { useRouter } from "expo-router";
 
 import styles from "./popularjobs.style";
 import { COLORS, SIZES } from "../../../constants";
@@ -18,10 +18,15 @@ export default function Popularjobs() {
 
   const { data, isLoading, error } = useFetch("search", {
     query: "React developer",
-    num_pages: 1,
+    num_pages: "1",
   });
 
   const [selectedJob, setSelectedJob] = useState();
+
+  const handleCardPress = (item) => {
+    router.push(`/job-details/${item.job_id}`);
+    setSelectedJob(item.job_id);
+  };
 
   return (
     <View style={styles.container}>
@@ -34,14 +39,18 @@ export default function Popularjobs() {
 
       <View style={styles.cardsContainer}>
         {isLoading ? (
-          <ActivityIndicator size="large" colors={COLORS.primary} />
+          <ActivityIndicator size="large" color={COLORS.primary} />
         ) : error ? (
           <Text>Something went wrong</Text>
         ) : (
           <FlatList
             data={data}
             renderItem={({ item }) => (
-              <PopularJobCard item={item} selectedJob={selectedJob} />
+              <PopularJobCard
+                item={item}
+                selectedJob={selectedJob}
+                handleCardPress={handleCardPress}
+              />
             )}
             keyExtractor={(item) => item.job_id}
             contentContainerStyle={{ columnGap: SIZES.medium }}
